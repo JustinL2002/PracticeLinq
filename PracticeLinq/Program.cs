5ed327a2f1ae45d7c302ace6c8a3f8ff4252984e
Justin Lawson <jmlawson2002@gmail.com>
2022-09-30 14:24:32 -0400

    practice Linq

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,8 @@
+﻿List<string> games = new List<string>();
+games.Add("GTA");
+games.Add("Valorant");
+games.Add("DOTA");
+games.Add("CSGO");
+games.Add("Elden Ring");
+
+games.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
